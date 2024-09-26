f59455fe20b91d505e1ef014ae1a87d2e8d57b53
tracychiang25 <hsinyen19@gmail.com>
2024-09-26 15:57:41 +1000

    integrate aws Cognito service

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { secretKey } = require('../middlewares/auth');
 const { createUser, getUserByEmail } = require('../models/User');
+const { signUp, getAuthTokens, confirmUser, verifyToken } = require('../services/Cognito');
+
 
 const registerUser = async (req, res) => {
   const { email, username, password } = req.body;
@@ -16,14 +17,35 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ error: true, msg: "User already exists" });
     }
 
+    // Create user in Cognito
+    const signUpResponse = await signUp(username, password, email);
+    if(signUpResponse.error){
+      return res.status(400).json({ error: true, msg: signUpResponse.error });
+    }else(console.log("Successfully registered user!"));
+    
+    let groupName;
+    if(username.startsWith("admin")){
+      groupName = "admin";
+    }else{
+      groupName = "GeneralUsers"
+    }
+    groupUser(username, groupName);
+
+    // Confirm user
+    await confirmUser(username);
+    
+    // Get token
+    const tokens = await getAuthTokens(username, password);
+    const idToken = tokens.idToken;
+    const resp  = await verifyToken(idToken);
+    const userId = resp.sub;
+
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
-
-    const newUser = await createUser({ email, username, passwordHash });
-
-    const token = jwt.sign({ id: newUser.userId, username: newUser.username, email: newUser.email }, secretKey, { expiresIn: '30d' });
-
-    res.status(201).json({ error: false, msg: "User registered successfully", username: newUser.username, token });
+    // // // Create use in DynamoDB
+    const newUser = await createUser({ email, username, passwordHash, userId });
+    
+    res.status(201).json({ error: false, msg: "User registered successfully", username: username, token: idToken });
   } catch (err) {
     console.error('Error during user registration:', err.message);
     res.status(500).json({ error: true, msg: "Server error", err: err.message });
@@ -47,10 +69,28 @@ const loginUser = async (req, res) => {
     if (!match) {
       return res.status(401).json({ error: true, msg: "Password incorrect" });
     }
+    
+    
+    const username= user.username
+    // Get token
+    const response = await getAuthTokens(username, password);
+    console.log("RESPONSE", response);
+    const idToken = response.idToken;
+    
+    // Decode the token
+    const decodeToken = (token) => {
+      const base64Url = token.split('.')[1]; // Get the payload part of the token
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace URL-safe characters
+      const decodedPayload = Buffer.from(base64, 'base64').toString('utf-8'); // Decode base64 to a string
+      const payload = JSON.parse(decodedPayload); // Parse the decoded payload
+      return payload;
+    }
+    const decodedToken = decodeToken(idToken)
+    
+    // Extract expiration time from decodedToken
+    const expirationTime = decodedToken.exp;
 
-    const token = jwt.sign({ id: user.userId, username: user.username, email: user.email }, secretKey, { expiresIn: '24h' });
-
-    res.json({ token_type: "Bearer", token, expires_in: 86400, username: user.username });
+    res.json({ token_type: "Bearer", idToken, expires_in: expirationTime, username: useer });
   } catch (err) {
     res.status(500).json({ error: true, msg: "Server error", err: err.message });
   }
